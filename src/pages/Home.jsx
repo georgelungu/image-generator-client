519876d7c130aaecea1f69bcdcd64d6ddcc79654
George Lungu <george.lungu1896@gmail.com>
2023-11-05 20:21:54 +0200

    i really don't know what has been modified

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,15 @@ import React, { useState, useEffect } from "react";
 
 import { Loader, Card, FormField } from "../components";
 
-const RenderCards = ({ data, title }) =>
-{
-  if (data?.length > 0)
-  {
-    return data.map((post) => <Card key={post._id} {...post}/>)
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />);
   }
 
   return (
-    <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">
-      {title}
-    </h2>
-  )
-}
+    <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
+  );
+};
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
@@ -46,20 +42,15 @@ const Home = () => {
           <>
             {searchText && (
               <h2 className="font-medium text-[#666e75] text-xl mb-3">
-                Showing results for<span className="text-[#222328">{searchText}</span>
+                Showing results for
+                <span className="text-[#222328">{searchText}</span>
               </h2>
             )}
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
-                <RenderCards 
-                  data={[]}
-                  title="No search results found"
-                />
+                <RenderCards data={[]} title="No search results found" />
               ) : (
-                <RenderCards 
-                  data={[]}
-                  title="No posts found"
-                />
+                <RenderCards data={[]} title="No posts found" />
               )}
             </div>
           </>
